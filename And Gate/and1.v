ad9aa2201f2e33327d57094df094eee1cf11e476
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2020-01-20 19:43:14 +0530

    Add to Verilog

---
+++ And Gate/and1.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/20/2020 07:11:03 PM
+// Design Name: 
+// Module Name: and1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and1(
+    input a,
+    input b,
+    output c
+    );
+    assign c= a&b;
+endmodule
