b7aedab1d3a9cd9b589cc9b8cf7d5c2938d4dc82
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2020-01-21 15:37:36 +0530

    Add to Verilog

---
+++ OR Gate/or1.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/20/2020 07:48:57 PM
+// Design Name: 
+// Module Name: or1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module or1(
+    input a,
+    input b,
+    output c
+    );
+    assign c= a|b;
+endmodule
