1b29287ba153ae9a8e6e56d151ef509c82650136
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2020-01-23 19:03:28 +0530

    Add to Verilog

---
+++ not1.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/22/2020 03:22:27 PM
+// Design Name: 
+// Module Name: not1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module not1(a,b);
+input a;
+output b;
+    assign b=~a;
+endmodule
