0ca56601be87fb26d6c9c022eb7161116cb46192
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2020-06-18 14:10:30 +0530

    Add to Verilog

---
+++ Summer Practice Problems/Counter/Up counter with load/Test bench.v
@@ -0,0 +1,34 @@
+module counter_4b_tb();
+reg clk,load;
+reg  [3:0] d;
+wire [3:0] q;
+
+counter_4b cnt(clk,load,d,q);
+
+initial
+    begin
+        #10
+        clk = 0;
+        load = 0;
+        d = 4'b1001;
+        #40
+        load = 1;
+        
+        #60
+        load = 0;
+       
+        
+        #200
+        $finish;
+    end
+
+
+initial
+    begin    
+        forever
+            begin
+                #5 clk=~clk;    
+            end
+    end
+
+endmodule
